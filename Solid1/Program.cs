23345189b23c657ebc4b7ea599e25e7c790904d2
MaxSalut <m.bezverkhyi28@gmail.com>
2024-11-22 22:17:56 +0200

    Add project files.

---
+++ Solid1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+//
+//клас Order відповідає за надто багато функцій: роботу із замовленням, відображення замовлення, а також операції збереження/завантаження.
+//Щоб виправити це порушення, ми можемо розділити відповідальності класу Order на окремі класи
+namespace Solid1
+{
+    class Item
+    {
+    }
+
+    // Відповідає за логіку управління замовленням
+    class Order
+    {
+        private List<Item> itemList;
+
+        public Order()
+        {
+            itemList = new List<Item>();
+        }
+
+        public List<Item> ItemList
+        {
+            get { return itemList; }
+        }
+
+        public void CalculateTotalSum() { /*...*/ }
+        public void GetItems() { /*...*/ }
+        public void GetItemCount() { /*...*/ }
+        public void AddItem(Item item) { /*...*/ }
+        public void DeleteItem(Item item) { /*...*/ }
+    }
+
+    // Відповідає за виведення замовлення
+    class OrderViewer
+    {
+        public void PrintOrder(Order order) { /*...*/ }
+        public void ShowOrder(Order order) { /*...*/ }
+    }
+
+    // Відповідає за збереження/завантаження замовлень
+    class OrderRepository
+    {
+        public void Load(Order order) { /*...*/ }
+        public void Save(Order order) { /*...*/ }
+        public void Update(Order order) { /*...*/ }
+        public void Delete(Order order) { /*...*/ }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
---
+++ Solid2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+//У цьому коді порушено принцип єдиного обов’язку. Клас EmailSender виконує дві задачі: Відправляє email таЛогує інформацію про відправлення через консоль.
+//Щоб виправити це, необхідно винести логування в окремий клас. Таким чином, клас EmailSender відповідатиме лише за відправлення email.
+class Email
+{
+    public string Theme { get; set; }
+    public string From { get; set; }
+    public string To { get; set; }
+}
+
+
+class Logger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+class EmailSender
+{
+    private readonly Logger _logger;
+
+    public EmailSender(Logger logger)
+    {
+        _logger = logger;
+    }
+
+    public void Send(Email email)
+    {
+        // ... sending ...
+        _logger.Log($"Email from '{email.From}' to '{email.To}' was sent");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Email e1 = new Email() { From = "Me", To = "Vasya", Theme = "Who are you?" };
+        Email e2 = new Email() { From = "Vasya", To = "Me", Theme = "vacuum cleaners!" };
+        Email e3 = new Email() { From = "Kolya", To = "Vasya", Theme = "No! Thanks!" };
+        Email e4 = new Email() { From = "Vasya", To = "Me", Theme = "washing machines!" };
+        Email e5 = new Email() { From = "Me", To = "Vasya", Theme = "Yes" };
+        Email e6 = new Email() { From = "Vasya", To = "Petya", Theme = "+2" };
+
+        Logger logger = new Logger();
+        EmailSender es = new EmailSender(logger);
+
+        es.Send(e1);
+        es.Send(e2);
+        es.Send(e3);
+        es.Send(e4);
+        es.Send(e5);
+        es.Send(e6);
+
+        Console.ReadKey();
+    }
+}
